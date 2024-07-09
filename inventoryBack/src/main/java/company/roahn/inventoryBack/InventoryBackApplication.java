af19bc5337e64b7ac841fc8f2f440ecc41482eaa
Anderson Cusma <andersoncusma20@gmail.com>
2024-07-08 23:28:40 -0500

    Initial Project

---
+++ inventoryBack/src/main/java/company/roahn/inventoryBack/InventoryBackApplication.java
@@ -0,0 +1,13 @@
+package company.roahn.inventoryBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryBackApplication.class, args);
+	}
+
+}
---
+++ inventoryBack/src/test/java/company/roahn/inventoryBack/InventoryBackApplicationTests.java
@@ -0,0 +1,13 @@
+package company.roahn.inventoryBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
